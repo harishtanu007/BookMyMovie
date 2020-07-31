20494f01cd55c60c04c5b78b6059c63fd42f9d7a
HarishKunta <33089746+harishkunta@users.noreply.github.com>
2020-07-31 14:31:42 -0400

    final

--- index.php
+++ index.php
@@ -112,15 +112,13 @@ include 'header.php';
       </div>
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+    
     <script src="js/jquery.min.js"></script>
     <script src="js/main.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    
     <script src="js/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+   
     <script src="js/ie10-viewport-bug-workaround.js"></script>
     
   </body>
